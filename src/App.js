bd750e2fe084ead6371c9704123958cf2fcd72eb
Charles Buxton <chazbux@gmail.com>
2020-07-21 12:44:39 -0600

    coming soon page for deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo"></div>
+      <div className="text">
+        <h1>CUSTOM WEB SOLUTIONS</h1>
+        <h2>COMING SOON</h2>
+        <a href="mailto: charles@grizzcode.com">Email Me</a>
+        
+      </div>
+      
     </div>
   );
 }
